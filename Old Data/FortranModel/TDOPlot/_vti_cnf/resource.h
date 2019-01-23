6d3d77385d3a664058fab7e94549f257ef86a754
Jordan Sosnowski <jjs@auburn.edu>
2019-01-23 13:20:09 -0600

    Removed Old Data to See if IBM Cloud Would Accept

--- Old Data/FortranModel/TDOPlot/_vti_cnf/resource.h
+++
@@ -1,7 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|18 Apr 2002 02:00:19 -0000
-vti_extenderversion:SR|5.0.2.2623
-vti_lineageid:SR|{3C854CE0-D962-4389-8165-CFDF810FAA94}
-vti_cacheddtm:TX|18 Apr 2002 02:00:19 -0000
-vti_filesize:IR|411
-vti_backlinkinfo:VX|
