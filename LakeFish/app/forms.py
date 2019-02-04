6e2d65c55b29375f154a75744e74dc91195e2f96
robin ward <robin@steelcitysites.net>
2019-02-03 20:45:11 -0600

    login page work

--- LakeFish/app/forms.py
+++ LakeFish/app/forms.py
@@ -8,27 +8,36 @@ from django.utils.translation import ugettext_lazy as _
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
-    firstName = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'First Name'}))
-    lastName = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Last Name'}))
-    occupation = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Occupation'}))
-    company = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Company'}))
-    email = forms.CharField(max_length=254,
+
+    username = forms.CharField(max_length=254,
                             widget=forms.TextInput({
                                 'class': 'form-control',
-                                'placeholder': 'Email/Username'}))
+                                'placeholder': 'User name'}))
     password = forms.CharField(label=_("Password"),
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+    #firstName = forms.CharField(max_length=254,
+    #                           widget=forms.TextInput({
+    #                               'class': 'form-control',
+    #                               'placeholder': 'First Name'}))
+    #lastName = forms.CharField(max_length=254,
+    #                           widget=forms.TextInput({
+    #                               'class': 'form-control',
+    #                               'placeholder': 'Last Name'}))
+    #occupation = forms.CharField(max_length=254,
+    #                           widget=forms.TextInput({
+    #                               'class': 'form-control',
+    #                               'placeholder': 'Occupation'}))
+    #company = forms.CharField(max_length=254,
+    #                           widget=forms.TextInput({
+    #                               'class': 'form-control',
+    #                               'placeholder': 'Company'}))
+    #email = forms.CharField(max_length=254,
+    #                        widget=forms.TextInput({
+    #                            'class': 'form-control',
+    #                            'placeholder': 'Email/Username'}))
+    #password = forms.CharField(label=_("Password"),
+    #                           widget=forms.PasswordInput({
+    #                               'class': 'form-control',
+    #                               'placeholder':'Password'}))
