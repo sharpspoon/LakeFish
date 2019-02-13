e23b789a27209b2bee18ce8537b955b7c199de3e
robin ward <robin@steelcitysites.net>
2019-02-13 17:04:28 -0600

    added a virtual environment. This is necessary to get configured for pushing to AU server

--- LakeFish/LakeFish/settings.py
+++ LakeFish/LakeFish/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'LakeFish.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'lakefish',
-        'USER': 'lakefishsa',
-        'PASSWORD': 'TJKDUPVINBDOABBS',
-        'HOST': 'lakefish.steelcitysites.net',   # Or an IP Address that your DB is hosted on
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', 
+#        'NAME': 'lakefish',
+#        'USER': 'lakefishsa',
+#        'PASSWORD': 'TJKDUPVINBDOABBS',
+#        'HOST': 'lakefish.steelcitysites.net',   # Or an IP Address that your DB is hosted on
+#        'PORT': '',
+#    }
+#}
 
 
 # Password validation
---
+++ LakeFish/env/Scripts/pyjade-script.py
@@ -0,0 +1,10 @@
+#!C:\lake\LakeFish\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pyjade==2.2.0','console_scripts','pyjade'
+__requires__ = 'pyjade==2.2.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('pyjade==2.2.0', 'console_scripts', 'pyjade')()
+    )
