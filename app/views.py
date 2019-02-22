c2d4c35725ec360dad00d07d55d9a4b6e9b52967
Robin Ward <robin@steelcitysites.net>
2019-02-22 14:02:19 -0600

    sln file fixed

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-from Weather import WeatherScraper as ws
+from weather import WeatherScraper as ws
 
 
 def home(request):
