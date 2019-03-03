ed604fbb07438659bf4a46b9f5b4dabee5281bdf
Cameron Taylor <kiba1996@gmail.com>
2019-03-03 15:12:49 -0600

    Set up init methods and changed the homepage so its not the base inputs

--- LakeFish/createLakeInitFile.py
+++ LakeFish/createLakeInitFile.py
@@ -9,7 +9,24 @@ if __name__ == '__main__':
 
 def createInitFile(parm={}):
     pass
-
+def gatherPost():
+    return
+    #takes in the post from the user and saves them as indevidual variables that can be accessed later
+def createInit():
+    return
+#creates the file of init and then begins populating it line by line with the required information also using the variables we saved from GatherPost
+def executeSim():
+    return
+#calls the fortran model to run and point to the init that we just created
+def destroyInit():
+    return
+#(Might not be needed) deletes the init file created by CreateInit
+def gatherOutput():
+    return
+#Takes the file created by ExecuteSim and parses it for the data that we need (either as an arraylist or dictionary)
+def displayOutput():
+    return
+#Displays the parsed data gathered by GatherOutput
 def handleStationName(cityName, stateName):
     statFile =  open("D:\Lake2019\#common\FIXED_INPUT\station.dat")
     stationList = []
