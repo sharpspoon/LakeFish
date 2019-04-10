44f5b6740cb376790be7a2fb30a443a37ae3ae02
Jordan Sosnowski <jjs0029@auburn.edu>
2019-04-10 12:34:49 -0500

    Added code to calc solar radition to main script
    need to pass correct parameters to it before it will work

--- weather/weatherscraper.py
+++ weather/weatherscraper.py
@@ -176,42 +176,43 @@ class WeatherScraper:
             day_counter += 1
         return weather_info
 
-    def jday_calc(self, i):
+    def jday_calc(self, day, hour):
         # Value used in calculation of solar raditaion
-        date_string = self.month + "/" + str(i) + "/" + self.year
-        date = datetime.strptime(date_string, "%m/%d/%Y")
-        # TEXT(W2-DATE(YEAR(W2),1,0),"000") -> calculates days from start of year
-        days = date.strftime('%j')
-        # RIGHT(YEAR(W2),2) -> Just grabs the year number
-        year = date_string[-2:]
-        formated = year + days
-        return formated
+        unformatted_date = self.year + self.month + day
+        date = datetime.strptime(unformatted_date, "%Y%m%d")
+        days = date.strftime("%j")
 
-    def solar_rad_cal(self, solar_cov, i):
-        # Calculates Solar Radiation
+        jday1 = float(days) - 1 + (float(hour) / 24)
+
+        return round(jday1, 2)
+
+    def solar_rad_cal(self, solcov, day, hour):
         slong = self.lng
         slat = self.lat
         pi = 3.1419
         phi = 15 * round(slong / 15.0)
 
-        jday1 = self.jday_calc(i)
-        hcloud = solar_cov
-        eqt = 0.17 * sin(4 * pi * (int(jday1) - 80) / 373) - \
-            0.129 * sin(2 * pi * int((jday1) - 8) / 355)
+        jday1 = self.jday_calc(day, hour)
+        hcloud = solcov
+        pi = 3.1419
+        eqt = 0.17 * sin(4 * pi * (int(jday1) - 80) / 373) - 0.129 * sin(2 * pi * int((jday1) - 8) / 355)
         hour1 = round(24 * (jday1 - int(jday1)))  # good
-        hh = round(2 * pi / 24 * (hour1 - ((slong - phi) * ong - phi)**24 / 360) + eqt - 12.0, 6)  # good
-        taud=2 * pi * (int(jday1) - 1) / 365
-        val=round(0.006918 - 0.399912 * cos(taud) + 0.070257 * sin(taud) - 0.006758
-                    * cos(2 * taud) + 0.000907 * sin(2 * taud) - 0.002697 * cos(3 * taud) + 0.00148 * sin(3 * taud), 7)
-        ao=asin(sin(slat * 0.01743) * sin(val) +
-                  cos(slat * 0.01743) * cos(val) * cos(hh))
+        hh = round(2 * pi / 24 * (hour1 - ((slong - phi) * 24 / 360) + eqt - 12.0), 6)
+        taud = 2 * pi * (int(jday1) - 1) / 365
+        val = round(0.006918 - 0.399912 * cos(taud) + 0.070257 * sin(taud) - 0.006758 * cos(2 * taud) + 0.000907 * sin(2 * taud) - 0.002697 * cos(3 * taud) + 0.00148 * sin(3 * taud), 7)
+        f_sin = sin(slat * 0.01743)
+        d_sin = sin(val)
+        s_cos = cos(slat * 0.01743)
+        d_cos = cos(val)
+        h_cos = cos(hh)
+        a_asin = f_sin * d_sin + s_cos * d_cos * h_cos
+        ao = asin(a_asin)
         ao = ao * 180 / pi
         if ao < 0:
             phis = 0
             sro = 0
         else:
-            phis = 24 * (2.044 * ao + 0.1296 * ao**2 - 1.941 *
-                         0.001 * ao**3 + 7.591 * 0.000001 * ao**4) * 0.1314
+            phis = 24 * (2.044 * ao + 0.1296 * ao**2 - 1.941 * 0.001 * ao**3 + 7.591 * 0.000001 * ao**4) * 0.1314
             sro = (1 - 0.65 * (hcloud)**2) * phis
         return sro
 
